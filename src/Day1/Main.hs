43ad1f44ab54512004018dc86b135f022a1be7c6
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-01 20:04:44 -0500

    Refactoring

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -6,19 +6,18 @@ numIncreases :: SonarSweepReport -> Int
 numIncreases [] = 0
 numIncreases [_] = 0
 numIncreases (x:xs) = snd $ foldl f (x, 0) xs
- where
-   f (x1, n) x2 = (x2, if x2 > x1 then succ n else n)
-
-compressInputs :: SonarSweepReport -> SonarSweepReport
-compressInputs report@(x1:x2:x3:xs) = go report
   where
-    go (x1:x2:x3:xs) = (x1+x2+x3) : go (x2:x3:xs)
-    go xs = []
-compressInputs _ = error "invariant violated: input too small"
+    f (x1, n) x2 = (x2, if x2 > x1 then succ n else n)
+
+interpolate :: SonarSweepReport -> SonarSweepReport
+interpolate report@(_:_:_:_) = interpolate' report
+  where
+    interpolate' (x1:x2:x3:xs) = (x1+x2+x3) : interpolate' (x2:x3:xs)
+    interpolate' xs = []
+interpolate _ = error "invariant violated: input too small"
 
 main :: IO ()
 main = do
-  let parse :: String -> SonarSweepReport
-      parse = fmap read . lines
+  let parse = fmap read . lines
   input <- parse <$> getContents
-  print $ numIncreases $ compressInputs input
+  print $ numIncreases $ interpolate input
