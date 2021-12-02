60a543f1577ab0fd48a6f48ecc33f6b1223c0a4c
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-01 19:21:43 -0500

    Day1 part 2

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -9,9 +9,16 @@ numIncreases (x:xs) = snd $ foldl f (x, 0) xs
  where
    f (x1, n) x2 = (x2, if x2 > x1 then succ n else n)
 
+compressInputs :: SonarSweepReport -> SonarSweepReport
+compressInputs report@(x1:x2:x3:xs) = go report
+  where
+    go (x1:x2:x3:xs) = (x1+x2+x3) : go (x2:x3:xs)
+    go xs = []
+compressInputs _ = error "invariant violated: input too small"
+
 main :: IO ()
 main = do
   let parse :: String -> SonarSweepReport
       parse = fmap read . lines
   input <- parse <$> getContents
-  print $ numIncreases input
+  print $ numIncreases $ compressInputs input
