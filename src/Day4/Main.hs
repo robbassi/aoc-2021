0ab2f76f8c37aa9ddceb55f042763ad396b3c348
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-04 18:11:15 -0500

    Initial commit for day 4, part 1

---
+++ src/Day4/Main.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE NamedFieldPuns, TupleSections #-}
+
+module Main where
+
+import Data.Maybe (fromJust)
+import Data.List (find)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.List.Split (splitOn)
+
+type DrawnNumbers = [Int]
+type Pos = (Int, Int)
+
+data Board = Board
+  { marked :: Map Pos Bool,
+    numMap :: Map Int Pos
+  }
+  deriving Show
+
+emptyBoard :: Board
+emptyBoard = Board
+  { marked = M.empty,
+    numMap = M.empty
+   }
+
+parseDrawnNumbers :: String -> DrawnNumbers
+parseDrawnNumbers = fmap read . splitOn ","
+
+parseBoards :: [String] -> [Board]
+parseBoards [] = []
+parseBoards (_:row1:row2:row3:row4:row5:rest) = board : parseBoards rest
+  where
+    numMap' = foldl f M.empty $ zip [0..] [row1, row2, row3, row4, row5]
+    f numMap (rowNum, numbers) = foldl g numMap $ zip [0..] $ words numbers
+      where
+        g numMap (colNum, number) = 
+          let n = read number
+          in M.insert n (rowNum, colNum) numMap
+    board = emptyBoard { numMap = numMap' }
+
+checkRow :: Int -> Board -> Bool
+checkRow n Board {..} = foldl checkRow' True [0..4]
+  where
+    checkRow' cond col = M.findWithDefault False (n, col) marked && cond
+
+checkCol :: Int -> Board -> Bool
+checkCol n Board {..} = foldl checkCol' True [0..4]
+  where
+    checkCol' cond row = M.findWithDefault False (row, n) marked && cond
+
+computeWinner :: DrawnNumbers -> [Board] -> (Board, Int)
+computeWinner drawnNumbers boards = fromJust $ snd $ foldl processNumber (boards, Nothing) drawnNumbers
+  where
+    processNumber answer@(_, Just _) _ = answer
+    processNumber (boards, Nothing) number = (boards', (,number) <$> find winner boards')
+      where
+        boards' = updateBoard <$> boards
+        updateBoard Board {..} = Board {marked = marked', ..}
+          where
+            marked' = case M.lookup number numMap of
+              Just (row, col) -> M.insert (row, col) True marked
+              Nothing -> marked
+        winner board = case M.lookup number $ numMap board of
+          Just (row, col) -> checkRow row board || checkCol col board
+          Nothing -> False
+
+computeScore :: (Board, Int) -> Int
+computeScore (Board {..}, winningNumber) = unmarkedSum * winningNumber
+  where
+    unmarkedSum = sum (entryToNum <$> M.toList numMap)
+    entryToNum (number, pos) = if M.findWithDefault False pos marked
+      then 0
+      else number
+
+main :: IO ()
+main = do
+  (drawnNumberStr : boardStrs) <- lines <$> getContents
+  let drawnNumbers = parseDrawnNumbers drawnNumberStr
+      boards = parseBoards boardStrs
+      winner = computeWinner drawnNumbers boards
+      score = computeScore winner
+  -- print $ "drawnNumbers = " ++ show drawnNumbers
+  -- print $ "boards = " ++ show boards
+  -- print $ "winner = " ++ show winner
+  print $ "score = " ++ show score
