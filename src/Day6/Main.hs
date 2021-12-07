63b75198c3762e6082cf4f9f4dd080e577558a36
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-07 01:13:38 -0500

    Day 6 (#5)

---
+++ src/Day6/Main.hs
@@ -0,0 +1,61 @@
+module Main where
+
+import Data.List.Split (splitOn)
+
+data School = School
+  { zero :: Int,
+    one :: Int,
+    two :: Int,
+    three :: Int,
+    four :: Int,
+    five :: Int,
+    six :: Int,
+    seven :: Int,
+    eight :: Int
+  }
+  deriving (Show)
+
+emptySchool :: School
+emptySchool = School 0 0 0 0 0 0 0 0 0
+
+simulateDay :: School -> School
+simulateDay School {..} =
+  School
+    { eight = zero,
+      seven = eight,
+      six = zero + seven,
+      five = six,
+      four = five,
+      three = four,
+      two = three,
+      one = two,
+      zero = one
+    }
+
+simulateDays :: Int -> School -> School
+simulateDays 0 school = school
+simulateDays n school = simulateDays (pred n) (simulateDay school)
+
+initSchool :: [Int] -> School
+initSchool = foldl count emptySchool
+  where
+    count s@School {..} n =
+      case n of
+        0 -> s {zero = succ zero}
+        1 -> s {one = succ one}
+        2 -> s {two = succ two}
+        3 -> s {three = succ three}
+        4 -> s {four = succ four}
+        5 -> s {five = succ five}
+        6 -> s {six = succ six}
+
+schoolSize :: School -> Int
+schoolSize School {..} = zero + one + two + three + four + five + six + seven + eight
+
+main :: IO ()
+main = do
+  [input] <- lines <$> getContents
+  let fish = read <$> splitOn "," input
+      school = initSchool fish
+  print $ "part 1 = " ++ show (schoolSize $ simulateDays 80 school)
+  print $ "part 2 = " ++ show (schoolSize $ simulateDays 256 school)
