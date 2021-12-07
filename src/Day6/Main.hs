5838a5b09421ccfd1ffe3c2fa8ec1322bad652a8
amackillop <austin.mackillop@gmail.com>
2021-12-06 19:57:02 -0800

    Use map in Day6

--- src/Day6/Main.hs
+++ src/Day6/Main.hs
@@ -1,42 +1,41 @@
-{-# LANGUAGE RecordWildCards, NamedFieldPuns #-}
-
 module Main where
 
 import Common
-import Data.Map (toList, fromListWith)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Maybe (fromMaybe)
 
 type Days = Int
+
 type Counter = Int
+
 type Amount = Int
-type School = [(Counter, Amount)]
 
-type Spawn = (Int, Int)
-
-data Generation = Generation {school :: School, children :: [Spawn]} deriving Show
+type School = Map Counter Amount
 
 parseSchool :: String -> School
-parseSchool = count . parseIntList
+parseSchool = countMap . parseIntList
 
-count :: Ord a => [a] -> [(a, Int)]
-count xs = toList (fromListWith (+) [(x, 1) | x <- xs])
+countMap :: Ord a => [a] -> Map a Int
+countMap xs = M.fromListWith (+) [(x, 1) | x <- xs]
 
 simulateDay :: School -> School
-simulateDay = map decrement . addSpawn
+simulateDay = M.mapKeysWith (+) decrement . addSpawn
   where
-    spawnCount school = countFish $ filter ((== 0) . fst) school
-    addSpawn school = let count = spawnCount school in if count == 0 then school else (9, count) : school
-    decrement (counter, amount) = (if counter < 7 then (counter - 1) `mod` 7 else counter - 1, amount)
+    spawnCount school = M.findWithDefault 0 0 school
+    addSpawn school = let count = spawnCount school in if count == 0 then school else M.insert 9 count school
+    decrement counter = if counter < 7 then (counter - 1) `mod` 7 else counter - 1
 
-simulate :: Int -> School -> School
+simulate :: Days -> School -> School
 simulate 0 school = school
 simulate days school = simulate (days - 1) (simulateDay school)
 
 countFish :: School -> Int
-countFish = sum . map snd
+countFish = sum . M.elems
 
 main :: IO ()
 main = do
   input <- readInput "src/Day6/input.txt"
   let school = parseSchool input
-  print $ countFish $ simulate 256 school
-  print $ length $ simulate 256 school
+  let total = countFish $ simulate 256 school
+  print $ if total == 1639643057051 then "PASS" else "FAIL"
