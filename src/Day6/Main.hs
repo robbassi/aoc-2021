4eb7deef81042b45aa13eb3c0f7157101569d94d
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-07 01:11:36 -0500

    Refactoring

--- src/Day6/Main.hs
+++ src/Day6/Main.hs
@@ -1,38 +1,32 @@
-{-# LANGUAGE RecordWildCards, NamedFieldPuns #-}
-
 module Main where
 
 import Data.List.Split (splitOn)
 
-
-type Days = Int
-
-type Spawn = (Int, Int)
-
 data School = School
-  { zero :: Int
-  , one :: Int
-  , two :: Int
-  , three :: Int
-  , four :: Int
-  , five :: Int
-  , six :: Int
-  , seven :: Int
-  , eight :: Int
+  { zero :: Int,
+    one :: Int,
+    two :: Int,
+    three :: Int,
+    four :: Int,
+    five :: Int,
+    six :: Int,
+    seven :: Int,
+    eight :: Int
   }
-  deriving Show
+  deriving (Show)
 
-simulateDay School {..} = School
-  { eight = zero
-  , seven = eight
-  , six = zero + seven
-  , five = six
-  , four = five
-  , three = four
-  , two = three
-  , one = two
-  , zero = one
-  }
+simulateDay School {..} =
+  School
+    { eight = zero,
+      seven = eight,
+      six = zero + seven,
+      five = six,
+      four = five,
+      three = four,
+      two = three,
+      one = two,
+      zero = one
+    }
 
 simulateDays :: Int -> School -> School
 simulateDays 0 school = school
@@ -41,20 +35,23 @@ simulateDays n school = simulateDays (pred n) (simulateDay school)
 initSchool :: [Int] -> School
 initSchool = foldl count (School 0 0 0 0 0 0 0 0 0)
   where
-    count s n =
+    count s@School {..} n =
       case n of
-        0 -> s { zero = zero s + 1 }
-        1 -> s { one = one s + 1 }
-        2 -> s { two = two s + 1 }
-        3 -> s { three = three s + 1 }
-        4 -> s { four = four s + 1 }
-        5 -> s { five = five s + 1 }
-        6 -> s { six = six s + 1 }
+        0 -> s {zero = succ zero}
+        1 -> s {one = succ one}
+        2 -> s {two = succ two}
+        3 -> s {three = succ three}
+        4 -> s {four = succ four}
+        5 -> s {five = succ five}
+        6 -> s {six = succ six}
+
+schoolSize :: School -> Int
+schoolSize School {..} = zero + one + two + three + four + five + six + seven + eight
 
 main :: IO ()
 main = do
   [input] <- lines <$> getContents
   let fish = read <$> splitOn "," input
-      School {..} = simulateDays 256 $ initSchool fish
-      size = zero + one + two + three + four + five + six + seven + eight
-  print $ "part 1 = " ++ show size
+      school = initSchool fish
+  print $ "part 1 = " ++ show (schoolSize $ simulateDays 80 school)
+  print $ "part 2 = " ++ show (schoolSize $ simulateDays 256 school)
