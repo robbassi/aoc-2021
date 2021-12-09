9a1c52c363bdf650fd75af9cd0d7aab62ea037dd
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-09 17:26:23 -0500

    Readability

--- src/Day8/Main.hs
+++ src/Day8/Main.hs
@@ -9,7 +9,6 @@ import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe (fromJust)
 import Data.Word (Word8)
-import Debug.Trace
 
 type Segment = Word8
 
@@ -32,14 +31,14 @@ data KnownDigits = KnownDigits
   }
   deriving (Show)
 
-data TranslationTable = TranslationTable
-  { a :: Segments,
-    b :: Segments,
-    c :: Segments,
-    d :: Segments,
-    e :: Segments,
-    f :: Segments,
-    g :: Segments
+data WiringTable = WiringTable
+  { wireA :: Segment,
+    wireB :: Segment,
+    wireC :: Segment,
+    wireD :: Segment,
+    wireE :: Segment,
+    wireF :: Segment,
+    wireG :: Segment
   }
   deriving (Show)
 
@@ -68,16 +67,16 @@ digitSegments =
     ]
 
 mkSegments :: String -> Segments
-mkSegments = foldl (flip markSegment) 0
+mkSegments = foldl (flip lightSegment) 0
 
-markSegment :: Char -> Segments -> Segments
-markSegment 'a' = (.|. segmentA)
-markSegment 'b' = (.|. segmentB)
-markSegment 'c' = (.|. segmentC)
-markSegment 'd' = (.|. segmentD)
-markSegment 'e' = (.|. segmentE)
-markSegment 'f' = (.|. segmentF)
-markSegment 'g' = (.|. segmentG)
+lightSegment :: Char -> Segments -> Segments
+lightSegment 'a' = (.|. segmentA)
+lightSegment 'b' = (.|. segmentB)
+lightSegment 'c' = (.|. segmentC)
+lightSegment 'd' = (.|. segmentD)
+lightSegment 'e' = (.|. segmentE)
+lightSegment 'f' = (.|. segmentF)
+lightSegment 'g' = (.|. segmentG)
 
 litSegments :: Segments -> Int
 litSegments segments = foldl countOnes 0 [0 .. 6]
@@ -91,13 +90,14 @@ lookup' k = fromJust . M.lookup k
 
 findKnownDigits :: [Segments] -> KnownDigits
 findKnownDigits patterns =
-  let one = lookup' 2 patternsByLength
+  let -- these are unique by length
+      one = lookup' 2 patternsByLength
       four = lookup' 4 patternsByLength
       seven = lookup' 3 patternsByLength
       eight = lookup' 7 patternsByLength
       -- the only six segment number whose length is 2 off from four is nine
       ([nine], zeroOrSix) = partition ((== 2) . litSegments . xor four) sixSegmentDigits
-      -- zero is a perfect superset of one, six is not
+      -- one is a subset of zero, but not six
       ([zero], [six]) = partition ((== one) . (.&. one)) zeroOrSix
    in KnownDigits {..}
   where
@@ -105,37 +105,43 @@ findKnownDigits patterns =
     indexByLength p = (litSegments p, p)
     sixSegmentDigits = filter ((== 6) . litSegments) patterns
 
-decodeTranslationTable :: [Segments] -> TranslationTable
-decodeTranslationTable patterns =
-  let a = seven `xor` one
-      b = d `xor` (four `xor` one)
-      c = eight `xor` six
-      d = eight `xor` zero
-      e = eight `xor` nine
-      f = eight `xor` (a .|. b .|. c .|. d .|. e .|. g)
-      g = e `xor` (eight `xor` (four .|. seven))
-   in TranslationTable {..}
+analyzeWiring :: [Segments] -> WiringTable
+analyzeWiring patterns =
+  let -- the only difference between 1 and 7 is a
+      wireA = seven `xor` one
+      -- if we remove 1 from 4, and then from d from that, we get b
+      wireB = d `xor` (four `xor` one)
+      -- c, d and e can be found by removing 6, 0 and 9 from 8 respectively
+      wireC = eight `xor` six
+      wireD = eight `xor` zero
+      wireE = eight `xor` nine
+      -- we can find f by removing all the others from 8
+      wireF = eight `xor` (wireA .|. wireB .|. wireC .|. wireD .|. wireE .|. wireG)
+      -- if we combine 4 and 7, then remove that from 8
+      -- we're left with g and e, then we remove e to get g
+      wireG = wireE `xor` (eight `xor` (four .|. seven))
+   in WiringTable {..}
   where
     KnownDigits {..} = findKnownDigits patterns
 
 decodeNote :: Note -> Int
-decodeNote Note {..} = foldl calculateDigit 0 digits
+decodeNote Note {..} = foldl decode 0 digits
   where
-    calculateDigit n digit = decodeDigit digit + n * 10
-    decodeDigit segments = lookup' decoded digitSegments
+    decode n digit = correctDigit digit + n * 10
+    correctDigit segments = lookup' rewired digitSegments
       where
-        decoded =
-          decodeSegment a segmentA
-            .|. decodeSegment b segmentB
-            .|. decodeSegment c segmentC
-            .|. decodeSegment d segmentD
-            .|. decodeSegment e segmentE
-            .|. decodeSegment f segmentF
-            .|. decodeSegment g segmentG
-        decodeSegment source target
-          | segments .&. source > 0 = target
+        rewired =
+          rewire wireA segmentA
+            .|. rewire wireB segmentB
+            .|. rewire wireC segmentC
+            .|. rewire wireD segmentD
+            .|. rewire wireE segmentE
+            .|. rewire wireF segmentF
+            .|. rewire wireG segmentG
+        rewire wire segment
+          | segments .&. wire > 0 = segment
           | otherwise = 0
-        TranslationTable {..} = decodeTranslationTable patterns
+        WiringTable {..} = analyzeWiring patterns
 
 countNumbersWithUniqueSegments :: [Segments] -> Int
 countNumbersWithUniqueSegments = foldl count 0
