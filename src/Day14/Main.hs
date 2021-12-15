a34e7b60f81a9afb68647e665b8fcae74c4dc376
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-14 23:52:00 -0500

    Simplify naming

--- src/Day14/Main.hs
+++ src/Day14/Main.hs
@@ -36,41 +36,41 @@ After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
 
 type InsertionRules = Map (Char, Char) Char
 
-type PairFrequencies = Map (Char, Char) Int
+type PairCounts = Map (Char, Char) Int
 
-type ElementFrequencies = Map Char Int
+type CharCounts = Map Char Int
 
-data Frequencies = Frequencies
-  { pairFrequencies :: PairFrequencies,
-    elementFrequencies :: ElementFrequencies
+data Counts = Counts
+  { pairCounts :: PairCounts,
+    charCounts :: CharCounts
   }
   deriving (Show)
 
-mkFrequencies :: String -> Frequencies
-mkFrequencies (t : template) =
-  let pairFrequencies = fst $ foldl updateCount (M.empty, t) template
-      elementFrequencies = foldl inc M.empty (t : template)
-   in Frequencies {..}
+mkCounts :: String -> Counts
+mkCounts (t : template) =
+  let pairCounts = fst $ foldl updateCount (M.empty, t) template
+      charCounts = foldl inc M.empty (t : template)
+   in Counts {..}
   where
     updateCount (m, a) b = (inc m (a, b), b)
     inc m k = M.insertWith (+) k 1 m
 
-simulate :: InsertionRules -> Frequencies -> Int -> Frequencies
+simulate :: InsertionRules -> Counts -> Int -> Counts
 simulate _ answer 0 = answer
-simulate rules Frequencies {..} steps = simulate rules next (pred steps)
+simulate rules Counts {..} steps = simulate rules next (pred steps)
   where
-    next = Frequencies pairFrequencies' elementFrequencies'
-    (pairs, elements) = foldl counts ([], []) $ M.toList pairFrequencies
-    pairFrequencies' = M.fromListWith (+) pairs
-    elementFrequencies' = foldl add elementFrequencies elements
-    counts (ps, es) ((a, b), n) =
+    next = Counts pairCounts' charCounts'
+    (pairs, chars) = foldl addCounts ([], []) $ M.toList pairCounts
+    pairCounts' = M.fromListWith (+) pairs
+    charCounts' = foldl add charCounts chars
+    addCounts (ps, cs) ((a, b), n) =
       let Just c = M.lookup (a, b) rules
-       in (((a, c), n) : ((c, b), n) : ps, (c, n) : es)
+       in (((a, c), n) : ((c, b), n) : ps, (c, n) : cs)
     add m (k, v) = M.insertWith (+) k v m
 
-computeAnswer :: Frequencies -> Int
-computeAnswer Frequencies {..} =
-  let quantity = sort $ snd <$> M.toList elementFrequencies
+computeAnswer :: Counts -> Int
+computeAnswer Counts {..} =
+  let quantity = sort $ snd <$> M.toList charCounts
    in last quantity - head quantity
 
 parseInsertionRules :: [String] -> InsertionRules
@@ -89,8 +89,8 @@ main = do
   let inputLines = lines input
       [[templateStr], insertionRulesStrs] = splitOn [""] inputLines
       insertionRules = parseInsertionRules insertionRulesStrs
-      startingFrequencies = mkFrequencies templateStr
-      tenSteps = simulate insertionRules startingFrequencies 10
-      fourtySteps = simulate insertionRules startingFrequencies 40
+      startingCounts = mkCounts templateStr
+      tenSteps = simulate insertionRules startingCounts 10
+      fourtySteps = simulate insertionRules startingCounts 40
   print $ "part 1 = " ++ show (computeAnswer tenSteps)
   print $ "part 2 = " ++ show (computeAnswer fourtySteps)
