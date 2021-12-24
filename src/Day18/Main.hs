c76f1b79257349a3e7ba1125dfa01fcabbb538fa
Rob Bassi <rob.bassi.89@gmail.com>
2021-12-23 21:39:50 -0500

    Refactoring

--- src/Day18/Main.hs
+++ src/Day18/Main.hs
@@ -2,59 +2,56 @@ module Main where
 
 import Data.Bool (bool)
 import Data.List (sort)
-import Debug.Trace
 
 type Depth = Int
 
 data Node = Parent Depth Node Node | Leaf Int
   deriving (Show, Eq, Read)
 
-nodeDepth :: Node -> Int
-nodeDepth (Parent d _ _) = d
-nodeDepth _ = 0
+depth :: Node -> Int
+depth (Parent d _ _) = d
+depth _ = 0
 
 succMaxDepth :: Node -> Node -> Int
-succMaxDepth l r = succ $ max (nodeDepth l) (nodeDepth r)
+succMaxDepth left right = succ $ max (depth left) (depth right)
 
 add :: Node -> Node -> Node
-add l@(Parent d1 _ _) r@(Parent d2 _ _) =
-  let d3 = succ (max d1 d2)
-   in reduce $ Parent d3 l r
+add left right = reduce $ Parent (succMaxDepth left right) left right
 
 explode :: Node -> Node
 explode root
-  | nodeDepth root == 5 = root'
+  | depth root == 5 = root'
   | otherwise = root
   where
     (root', _, _) = explode' root
-    explode' (Parent 2 (Parent 1 (Leaf a) (Leaf b)) r) =
-      (Parent (succ $ nodeDepth r) (Leaf 0) (incLeftMost b r), a, 0)
-    explode' (Parent 2 (Leaf l) (Parent 1 (Leaf a) (Leaf b))) =
-      (Parent 1 (Leaf $ l + a) (Leaf 0), 0, b)
-    explode' (Parent d l r)
-      | nodeDepth l >= nodeDepth r =
-        let (l', ld, rd) = explode' l
-         in (Parent (succMaxDepth l' r) l' (incLeftMost rd r), ld, 0)
+    explode' (Parent 2 (Parent 1 (Leaf a) (Leaf b)) right) =
+      (Parent (succ $ depth right) (Leaf 0) (incLeftMost b right), a, 0)
+    explode' (Parent 2 (Leaf left) (Parent 1 (Leaf a) (Leaf b))) =
+      (Parent 1 (Leaf $ left + a) (Leaf 0), 0, b)
+    explode' (Parent d left right)
+      | depth left >= depth right =
+        let (left', leftDelta, rightDelta) = explode' left
+         in (Parent (succMaxDepth left' right) left' (incLeftMost rightDelta right), leftDelta, 0)
       | otherwise =
-        let (r', ld, rd) = explode' r
-         in (Parent (succMaxDepth l r') (incRightMost ld l) r', 0, rd)
-    incLeftMost v (Parent d l r) = Parent d (incLeftMost v l) r
-    incLeftMost v (Leaf n) = Leaf $ n + v
-    incRightMost v (Parent d l r) = Parent d l (incRightMost v r)
-    incRightMost v (Leaf n) = Leaf $ n + v
+        let (right', leftDelta, rightDelta) = explode' right
+         in (Parent (succMaxDepth left right') (incRightMost leftDelta left) right', 0, rightDelta)
+    incLeftMost delta (Parent d left right) = Parent d (incLeftMost delta left) right
+    incLeftMost delta (Leaf v) = Leaf (v + delta)
+    incRightMost delta (Parent d left right) = Parent d left (incRightMost delta right)
+    incRightMost delta (Leaf v) = Leaf (v + delta)
 
 split :: Node -> Node
 split = snd . split'
   where
-    split' (Parent d l r) =
-      let (done, l') = split' l
+    split' (Parent d left right) =
+      let (done, left') = split' left
        in if done
-            then (done, Parent (succMaxDepth l' r) l' r)
+            then (done, Parent (succMaxDepth left' right) left' right)
             else
-              let (done, r') = split' r
+              let (done, right') = split' right
                in if done
-                    then (done, Parent (succMaxDepth l r') l r')
-                    else (done, Parent d l r)
+                    then (done, Parent (succMaxDepth left right') left right')
+                    else (done, Parent d left right)
     split' (Leaf v)
       | v >= 10 =
         let low = floor $ fromIntegral v / 2
@@ -63,41 +60,40 @@ split = snd . split'
       | otherwise = (False, Leaf v)
 
 reduce :: Node -> Node
-reduce n =
-  let n' = split $ reduceExplode n
-   in bool (reduce n') n' (n == n')
+reduce root = bool (reduce root') root' (root == root')
   where
-    reduceExplode n =
-      let n' = explode n
-       in bool (reduceExplode n') n' (n == n')
+    root' = split $ reduceExplode root
+    reduceExplode root =
+      let root' = explode root
+       in bool (reduceExplode root') root' (root == root')
 
 magnitude :: Node -> Int
-magnitude (Leaf n) = n
-magnitude (Parent _ l r) = 3 * magnitude l + 2 * magnitude r
+magnitude (Leaf root) = root
+magnitude (Parent _ left right) = 3 * magnitude left + 2 * magnitude right
 
 parseTree :: String -> Node
 parseTree str = traceDepth tree
   where
     tree = read $ transCode str
-    traceDepth n@(Parent _ l r) =
-      Parent (treeDepth n 0) (traceDepth l) (traceDepth r)
-    traceDepth n = n
+    traceDepth root@(Parent _ left right) =
+      Parent (treeDepth root 0) (traceDepth left) (traceDepth right)
+    traceDepth root = root
     treeDepth (Leaf _) d = d
-    treeDepth (Parent _ l r) d =
-      max (treeDepth l $ succ d) (treeDepth r $ succ d)
+    treeDepth (Parent _ left right) d =
+      max (treeDepth left $ succ d) (treeDepth right $ succ d)
 
 transCode :: String -> String
 transCode str = go str ""
   where
-    go [] r = r
-    go ('[' : s) r = go s (r ++ "(Parent 0 ")
-    go (',' : s) r = go s (r ++ " ")
-    go (']' : s) r = go s (r ++ ")")
-    go (v : s) r = go s (r ++ "(Leaf " ++ [v] ++ ")")
+    go [] res = res
+    go ('[' : s) res = go s (res ++ "(Parent 0 ")
+    go (',' : s) res = go s (res ++ " ")
+    go (']' : s) res = go s (res ++ ")")
+    go (v : s) res = go s (res ++ "(Leaf " ++ [v] ++ ")")
 
 showTree :: Node -> String
-showTree (Parent _ l r) = "[" ++ showTree l ++ "," ++ showTree r ++ "]"
-showTree (Leaf n) = show n
+showTree (Parent _ left right) = "[" ++ showTree left ++ "," ++ showTree right ++ "]"
+showTree (Leaf root) = show root
 
 roundTrip :: String -> Bool
 roundTrip s = s == showTree (parseTree s)
@@ -107,7 +103,7 @@ main = do
   input <- getContents
   let nums@(x : xs) = parseTree <$> lines input
       perms = add <$> nums <*> nums
+      sumMagnitude = magnitude $ foldl add x xs
       highestMagnitude = maximum $ magnitude <$> perms
-      total = foldl add x xs
-  print $ "part 1 = " ++ show (magnitude total)
+  print $ "part 1 = " ++ show sumMagnitude
   print $ "part 2 = " ++ show highestMagnitude
